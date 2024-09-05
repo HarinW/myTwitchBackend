95cc96b44683e634a3573e312c668ba0e53b4c7b
HarinW <su.junh@northeastern.edu>
2024-09-04 19:30:06 -0700

    upload

---
+++ src/main/java/com/laioffer/twitch/db/entity/FavoriteRecordEntity.java
@@ -0,0 +1,14 @@
+package com.laioffer.twitch.db.entity;
+
+import java.time.Instant;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("favorite_records")
+public record FavoriteRecordEntity(
+    @Id Long id,
+    Long userId,
+    Long itemId,
+    Instant createdAt
+) {
+}
